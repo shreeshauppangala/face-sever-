798a35a5553aebd72b8c2874981f0acf530c82bc
Shreesha Uppangala <shreeshauppangala@gmail.com>
2021-01-25 18:32:24 +0530

    database

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ const image = require('./controllers/image')
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-flexible-85917',
     user : 'postgres',
-    password : 'pace@123',
+    password : '',
     database : '_smart-brain_'
   }
 });
